9f5dab210c671514542bd704aa5ed99161e71f27
LuukvH <laj@home.nl>
2015-04-06 12:59:26 +0200

    GUI Changes

--- JoskeApp/app/src/main/java/software/laj/joskeapp/SoundItemFactory.java
+++ JoskeApp/app/src/main/java/software/laj/joskeapp/SoundItemFactory.java
@@ -10,31 +10,31 @@ public class SoundItemFactory {
     public static ArrayList<SoundItem> create(){
         ArrayList<SoundItem> soundItems = new ArrayList<SoundItem>(){};
 
-        soundItems.add(new SoundItem(R.string.sound001, R.raw.sound001, R.drawable.ic_pinguin));
-        soundItems.add(new SoundItem(R.string.sound002, R.raw.sound002, R.drawable.ic_pinguin));
-        soundItems.add(new SoundItem(R.string.sound003, R.raw.sound003, R.drawable.ic_pinguin));
-        soundItems.add(new SoundItem(R.string.sound004, R.raw.sound004, R.drawable.ic_pinguin));
-        soundItems.add(new SoundItem(R.string.sound005, R.raw.sound005, R.drawable.ic_pinguin));
-        soundItems.add(new SoundItem(R.string.sound006, R.raw.sound006, R.drawable.ic_pinguin));
-        soundItems.add(new SoundItem(R.string.sound007, R.raw.sound007, R.drawable.ic_pinguin));
-        soundItems.add(new SoundItem(R.string.sound008, R.raw.sound008, R.drawable.ic_pinguin));
-        soundItems.add(new SoundItem(R.string.sound009, R.raw.sound009, R.drawable.ic_pinguin));
-        soundItems.add(new SoundItem(R.string.sound010, R.raw.sound010, R.drawable.ic_pinguin));
-        soundItems.add(new SoundItem(R.string.sound011, R.raw.sound011, R.drawable.ic_pinguin));
-        soundItems.add(new SoundItem(R.string.sound012, R.raw.sound012, R.drawable.ic_pinguin));
-        soundItems.add(new SoundItem(R.string.sound013, R.raw.sound013, R.drawable.ic_pinguin));
-        soundItems.add(new SoundItem(R.string.sound014, R.raw.sound014, R.drawable.ic_pinguin));
-        soundItems.add(new SoundItem(R.string.sound015, R.raw.sound015, R.drawable.ic_pinguin));
-        soundItems.add(new SoundItem(R.string.sound016, R.raw.sound016, R.drawable.ic_pinguin));
-        soundItems.add(new SoundItem(R.string.sound017, R.raw.sound017, R.drawable.ic_pinguin));
-        soundItems.add(new SoundItem(R.string.sound018, R.raw.sound018, R.drawable.ic_pinguin));
-        soundItems.add(new SoundItem(R.string.sound019, R.raw.sound019, R.drawable.ic_pinguin));
-        soundItems.add(new SoundItem(R.string.sound020, R.raw.sound020, R.drawable.ic_pinguin));
-        soundItems.add(new SoundItem(R.string.sound021, R.raw.sound021, R.drawable.ic_pinguin));
-        soundItems.add(new SoundItem(R.string.sound022, R.raw.sound022, R.drawable.ic_pinguin));
-        soundItems.add(new SoundItem(R.string.sound023, R.raw.sound023, R.drawable.ic_pinguin));
-        soundItems.add(new SoundItem(R.string.sound024, R.raw.sound024, R.drawable.ic_pinguin));
-        soundItems.add(new SoundItem(R.string.sound025, R.raw.sound025, R.drawable.ic_pinguin));
+        soundItems.add(new SoundItem(R.string.sound001, R.raw.sound001, R.drawable.ic_sound));
+        soundItems.add(new SoundItem(R.string.sound002, R.raw.sound002, R.drawable.ic_sound));
+        soundItems.add(new SoundItem(R.string.sound003, R.raw.sound003, R.drawable.ic_sound));
+        soundItems.add(new SoundItem(R.string.sound004, R.raw.sound004, R.drawable.ic_sound));
+        soundItems.add(new SoundItem(R.string.sound005, R.raw.sound005, R.drawable.ic_sound));
+        soundItems.add(new SoundItem(R.string.sound006, R.raw.sound006, R.drawable.ic_sound));
+        soundItems.add(new SoundItem(R.string.sound007, R.raw.sound007, R.drawable.ic_sound));
+        soundItems.add(new SoundItem(R.string.sound008, R.raw.sound008, R.drawable.ic_sound));
+        soundItems.add(new SoundItem(R.string.sound009, R.raw.sound009, R.drawable.ic_sound));
+        soundItems.add(new SoundItem(R.string.sound010, R.raw.sound010, R.drawable.ic_sound));
+        soundItems.add(new SoundItem(R.string.sound011, R.raw.sound011, R.drawable.ic_sound));
+        soundItems.add(new SoundItem(R.string.sound012, R.raw.sound012, R.drawable.ic_sound));
+        soundItems.add(new SoundItem(R.string.sound013, R.raw.sound013, R.drawable.ic_sound));
+        soundItems.add(new SoundItem(R.string.sound014, R.raw.sound014, R.drawable.ic_sound));
+        soundItems.add(new SoundItem(R.string.sound015, R.raw.sound015, R.drawable.ic_sound));
+        soundItems.add(new SoundItem(R.string.sound016, R.raw.sound016, R.drawable.ic_sound));
+        soundItems.add(new SoundItem(R.string.sound017, R.raw.sound017, R.drawable.ic_sound));
+        soundItems.add(new SoundItem(R.string.sound018, R.raw.sound018, R.drawable.ic_sound));
+        soundItems.add(new SoundItem(R.string.sound019, R.raw.sound019, R.drawable.ic_sound));
+        soundItems.add(new SoundItem(R.string.sound020, R.raw.sound020, R.drawable.ic_sound));
+        soundItems.add(new SoundItem(R.string.sound021, R.raw.sound021, R.drawable.ic_sound));
+        soundItems.add(new SoundItem(R.string.sound022, R.raw.sound022, R.drawable.ic_sound));
+        soundItems.add(new SoundItem(R.string.sound023, R.raw.sound023, R.drawable.ic_sound));
+        soundItems.add(new SoundItem(R.string.sound024, R.raw.sound024, R.drawable.ic_sound));
+        soundItems.add(new SoundItem(R.string.sound025, R.raw.sound025, R.drawable.ic_sound));
 
         return soundItems;
     }
